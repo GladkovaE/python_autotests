3f5731563aefe82b60011bc0cbb12611c7c08c84
GladkovaE <delta2005.71@gmail.com>
2025-01-12 13:23:10 +0300

    Update test_pokemon.py

--- test_pokemon.py
+++ test_pokemon.py
@@ -1,7 +1,7 @@
 import requests
 
 URL = 'https://api.pokemonbattle.ru/v2'
-TOKEN = '9316663ac6d6f2e589f7928fe94e6473'
+TOKEN = 'абракадабра'
 HEADER = {'Content-Type' : 'application/json','trainer_token' : TOKEN}
 
 response = requests.get(url = f'{URL}/trainers?trainer_id=12742', headers = HEADER)
